4834269e3f323db100a09b9c9a613c9c44188286
Rafael Chagas Correia <rafaelccorreia@outlook.com>
2022-07-19 15:03:44 -0300

    Configurações inciais

--- pokedex/src/pages/detailsPoke.js
+++ pokedex/src/pages/detailsPoke.js
@@ -1,6 +1,26 @@
 import React from "react"
 import {useNavigate, useEffect} from 'react-router-dom'
 import {goToHome} from '../coordinator/coordinator'
+import styled from "styled-components"
+
+const HeaderContainer = styled.header` 
+    min-height: 16vh;
+    width: 100%;
+    background-color: gray;
+`
+
+const MainContainer = styled.main` 
+    width: 100%;
+    min-height: 84vh;
+    background-color: yellow;
+`
+
+const TituloPagina = styled.h2` 
+    font-size: 48px;
+    font-weight: 700;
+    font-family: 'Poppins';
+    margin-top : 2rem;
+`
 
 export function DetailsPage() {
     const navigate = useNavigate()
@@ -8,11 +28,14 @@ export function DetailsPage() {
 
     return(
         <div>
-            Detalhes
-            <button onClick={() => goToHome(navigate)}>todos pokemons</button>
+            <HeaderContainer>
+                <button onClick={() => goToHome(navigate)}>todos pokemons</button>
+            </HeaderContainer>
+            <MainContainer>
+                <TituloPagina>Detalhes</TituloPagina>
+
+            </MainContainer>
         </div>
     )
-
-    
 }
 
