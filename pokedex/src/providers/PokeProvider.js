758801d43b31095a4463fd308a41a663285b4572
Rafael Chagas Correia <rafaelccorreia@outlook.com>
2022-07-22 20:24:14 -0300

    Atualizando algumas coisas

--- pokedex/src/providers/PokeProvider.js
+++ pokedex/src/providers/PokeProvider.js
@@ -5,13 +5,47 @@ import axios from 'axios'
 
 export const PokeProvider = (props) => {
     // se necessário mudar os nomes dos estados e requisições
+    const [pokeList, setPokeList] = useState([])
+    const [pokeListDetails, setPokeListDetails] = useState([])
     const [detalhesPokemon, setDetalhesPokemon] = useState({})
     const [coresPokemon, setCoresPokemon] = useState([])
 
     //requisições
+    const getPokemon = () => {
+        axios
+            .get(
+                "https://pokeapi.co/api/v2/pokemon/"
+            )
+            .then((res) => {
+                // console.log(res.data)
+                setPokeList(res?.data?.results?.map(pokemon => {
+                    return pokemon.name
+                }))
+            })
+            .catch((error) => {
+                console.log(error.response)
+            })
+    }
+
+    const getAllPokemonDetails = () => {
+        pokeList?.map(pokemon => {
+            if(pokeListDetails.length < 21) {
+                axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+                .then(resp => {
+                    setPokeListDetails(prevPokeListDetails => [...prevPokeListDetails, resp.data])
+                    return resp.data
+                })
+                .catch(err => {
+                    console.log(err)
+                    return err.response
+                })
+            }
+        })
+    }
+    
     //pega um pokemon específico, vai receber o id por parâmetro na página de detalhes, ou onde for chamada
     const GetPokemonDetails = (idPokemon) => {
-        axios.get(`https://pokeapi.co/api/v2/pokemon/lugia/`)
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${idPokemon}/`)
         .then(resp => {
             setDetalhesPokemon(resp.data)
         })
@@ -36,7 +70,7 @@ export const PokeProvider = (props) => {
         }
     }
     
-    return <PokeContext.Provider value={{GetPokemonDetails, getAllPokemonColors, detalhesPokemon, coresPokemon}}>
+    return <PokeContext.Provider value={{getPokemon, getAllPokemonDetails, GetPokemonDetails, getAllPokemonColors, pokeList, pokeListDetails, detalhesPokemon, coresPokemon}}>
         {props.children}
     </PokeContext.Provider>
 }
