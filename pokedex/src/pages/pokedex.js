8836895b1169e19ee6e74b8bd129f28182fb546e
Mariluci de Lima Walczak <mariluciwalcza@gmail.com>
2022-07-22 21:12:09 -0300

    atualizando

--- pokedex/src/pages/pokedex.js
+++ pokedex/src/pages/pokedex.js
@@ -1,92 +1,85 @@
 import React from "react"
-import { useNavigate, useEffect } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axios from "react"
 import { goToDetailsPage, goToHome } from "../coordinator/coordinator"
-import styled, { createGlobalStyle } from "styled-components"
 import Logo from '../assets/logo.png'
 import Poke from '../assets/poke.png'
 import Pokemon from '../assets/pokemon.png'
 import { CardPoke } from "../components/CardPoke"
+import {Tela, GlobalStyle, Header, Imagem, Body, Titulo, Bloco} from './pokedexStyled'
 
-const GlobalStyle = createGlobalStyle`
-margin: 0;
-padding: 0;
-`
-
-const Tela = styled.div`
-display: flex;
-flex-direction: column;
-width: 100vw;
-`
-
-const Header = styled.div`
-min-height: 16vh;
-width: 100%;
-display: flex;
-justify-content: left;
-align-items: center;
-a{
-    font-size: 24px;
-    margin-left: 80px;
-}
-`
-
-const Imagem = styled.div`
-display: flex;
-align-items: center;
-margin-left: 290px;
-`
-
-const Body = styled.div`
-width: 100%;
-min-height:calc(100vh - 16vh);
-background:#5E5E5E;
-display: flex;
-flex-direction: column;
-`
-
-const Titulo = styled.h1`
-margin: 60px 40px;
-color: white;
-`
-
-const Bloco = styled.div`
-margin: 70px 30px;
-display: flex;
-flex-wrap: wrap;
-justify-content: flex-start;
-align-items: center;
-`
 
 export function Pokedex() {
+
     const navigate = useNavigate()
 
-    return (
-        <div>
-            <Tela>
-                <GlobalStyle />
-                <Header>
-                    <div>
-                        <a href="/">{"<"} Todos Pokémons</a>
-                    </div>
-                    <Imagem>
-                        <img src={Logo} />
-                    </Imagem>
-                </Header>
-                <Body>
-                    <Titulo>Todos Pokémons</Titulo>
-                    <Bloco>
-                       <CardPoke/>
-                       <CardPoke/>
-                       <CardPoke/>
-                       <CardPoke/>
-                       <CardPoke/>                     
+    const [pokeList, setPokeList] = useState([])
+    const [pokemons, setPokemons] = useState([])
+    const [name, setName] = useState([])
+    const [nr, setNr] = useState([])
 
-                    </Bloco>
-                </Body>
-            </Tela>
-        </div>
-    )
+    const getPokemon = () => {
+         axios
+             .get(
+                 "https://pokeapi.co/api/v2/pokemon/"
+             )
+             .then((res) => {
+                 // console.log(res.data)
+                 setPokeList(res.data.results)
+                 console.log(res.data.results)
+             })
+             .catch((error) => {
+                 console.log(error.response)
+             })
+     }
+
+        useEffect(() => {
+            getPokemon();
+        }, []);
+
+        const mapearPokemon = pokeList.map((pokemon) => {
+            return (pokemon)
+        }) 
 
 
-}
+      /*  const PokeIndiv = () 
+           
+                <PokeIndiv name={pokemon.name} url={pokemon.url} /> */ 
+
+
+
+
+
+
+
+        return (
+            <div>
+                <Tela>
+                    <GlobalStyle />
+                    <Header>
+                        <div>
+                            <a href="/">{"<"} Todos Pokémons</a>
+                        </div>
+                        <Imagem>
+                            <img src={Logo} />
+                        </Imagem>
+                    </Header>
+                    <Body>
+                        <Titulo>Todos Pokémons</Titulo>
+                        <Bloco>
+                            <CardPoke />
+                            <CardPoke />
+                            <CardPoke />
+                            <CardPoke />
+                            <CardPoke />
+
+                        </Bloco>
+                    </Body>
+                </Tela>
+            </div>
+        )
+
+
+    }
 
---
+++ pokedex/src/pages/pokedexStyled.js
@@ -0,0 +1,52 @@
+import styled, { createGlobalStyle } from "styled-components"
+
+
+export const GlobalStyle = createGlobalStyle`
+margin: 0;
+padding: 0;
+`
+
+export const Tela = styled.div`
+display: flex;
+flex-direction: column;
+width: 100vw;
+`
+
+export const Header = styled.div`
+min-height: 16vh;
+width: 100%;
+display: flex;
+justify-content: left;
+align-items: center;
+a{
+    font-size: 24px;
+    margin-left: 80px;
+}
+`
+
+export const Imagem = styled.div`
+display: flex;
+align-items: center;
+margin-left: 290px;
+`
+
+export const Body = styled.div`
+width: 100%;
+min-height:calc(100vh - 16vh);
+background:#5E5E5E;
+display: flex;
+flex-direction: column;
+`
+
+export const Titulo = styled.h1`
+margin: 60px 40px;
+color: white;
+`
+
+export const Bloco = styled.div`
+margin: 70px 30px;
+display: flex;
+flex-wrap: wrap;
+justify-content: flex-start;
+align-items: center;
+`
