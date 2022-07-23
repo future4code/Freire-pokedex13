4b65dde913424a2c90b41c31d965de6b15a57ce4
Mariluci de Lima Walczak <mariluciwalcza@gmail.com>
2022-07-23 16:48:55 -0300

    atualizando

--- pokedex/src/pages/pokedex.js
+++ pokedex/src/pages/pokedex.js
@@ -7,7 +7,7 @@ import Logo from '../assets/logo.png'
 import Poke from '../assets/poke.png'
 import Pokemon from '../assets/pokemon.png'
 import { CardPoke } from "../components/CardPoke"
-import {Tela, GlobalStyle, Header, Imagem, Body, Titulo, Bloco} from './pokedexStyled'
+import { Tela, GlobalStyle, Header, Imagem, Body, Titulo, Bloco } from './pokedexStyled'
 
 
 export function Pokedex() {
@@ -19,67 +19,61 @@ export function Pokedex() {
     const [name, setName] = useState([])
     const [nr, setNr] = useState([])
 
-    const getPokemon = () => {
-         axios
-             .get(
-                 "https://pokeapi.co/api/v2/pokemon/"
-             )
-             .then((res) => {
-                 // console.log(res.data)
-                 setPokeList(res.data.results)
-                 console.log(res.data.results)
-             })
-             .catch((error) => {
-                 console.log(error.response)
-             })
-     }
 
-        useEffect(() => {
-            getPokemon();
-        }, []);
+    useEffect(() => {
+        getPokemon();
+    }, []);
 
-        const mapearPokemon = pokeList.map((pokemon) => {
-            return (pokemon)
-        }) 
-
-
-      /*  const PokeIndiv = () 
-           
-                <PokeIndiv name={pokemon.name} url={pokemon.url} /> */ 
-
-
-
-
-
-
-
-        return (
-            <div>
-                <Tela>
-                    <GlobalStyle />
-                    <Header>
-                        <div>
-                            <a href="/">{"<"} Todos Pokémons</a>
-                        </div>
-                        <Imagem>
-                            <img src={Logo} />
-                        </Imagem>
-                    </Header>
-                    <Body>
-                        <Titulo>Todos Pokémons</Titulo>
-                        <Bloco>
-                            <CardPoke />
-                            <CardPoke />
-                            <CardPoke />
-                            <CardPoke />
-                            <CardPoke />
-
-                        </Bloco>
-                    </Body>
-                </Tela>
-            </div>
-        )
-
-
+    let listaPokemonJsx
+    if(pokeListDetails?.length === 20) {
+        listaPokemonJsx =  pokeListDetails?.map(pokemon => {   
+            return (
+                <CardPoke
+                    id={pokemon?.id}
+                    nome={pokemon?.name}
+                    key={pokemon?.id}
+                    imagem={pokemon?.sprites?.other?.home?.front_default}
+                    tipos={pokemon?.types}
+                />
+            )
+        })
     }
+
+   
+
+
+
+
+
+
+
+    return (
+        <div>
+            <Tela>
+                <GlobalStyle />
+                <Header>
+                    <div>
+                        <a href="/">{"<"} Todos Pokémons</a>
+                    </div>
+                    <Imagem>
+                        <img src={Logo} />
+                    </Imagem>
+                </Header>
+                <Body>
+                    <Titulo>Meus Pokémons</Titulo>
+                    <Bloco>
+                        <CardPoke />
+                        <CardPoke />
+                        <CardPoke />
+                        <CardPoke />
+                        <CardPoke />
+
+                    </Bloco>
+                </Body>
+            </Tela>
+        </div>
+    )
+
+
+}
 
